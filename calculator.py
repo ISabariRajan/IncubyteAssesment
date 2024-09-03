3f748277cce87f70248ab2365713dc72be1b62cb
Sabari Rajan <iamsabarirajan@gmail.com>
2024-09-03 19:08:40 +0530

    Added the ability to use custom delimiters except '-'

--- calculator.py
+++ calculator.py
@@ -1,12 +1,54 @@
+import re
+
 class NegativeNumberException(Exception):
     def __init__(self, number):
         super().__init__(f"negative numbers not allowed {number}")
 
+class InvalidDelimiterException(Exception):
+  def __init__(self, delimiter="-"):
+    super().__init__(f"Delimiter {delimiter} is not allowed")
+
+
 class StringCalculator:
+
+  def get_delimiters_and_numbers(self, numbers):
+    """
+    Extracts the delimiters and numbers from the input string 'numbers'.
+      Args:
+        numbers (str): The string containing delimiters and numbers.
+      
+      Returns:
+        tuple: A tuple containing the extracted delimiters and numbers.
+      
+      Raises:
+        InvalidDelimiterException: If the delimiters contain a hyphen (-) character.
+    """
+
+    delimiters = re.search(r"//[\D]+\n", numbers).group()
+    if "-" in delimiters:
+      raise InvalidDelimiterException()
+    numbers = numbers.replace(delimiters, "")
+    return delimiters, numbers
+
+  def split_numbers_with_delimiters(self, delimiters, numbers):
+    """
+    Splits the input string 'numbers' using the specified 'delimiters' as a delimiter.
+      Args:
+        delimiters (str): The character or sequence of characters to use as a delimiter.
+        numbers (str): The string containing numbers separated by the specified delimiters.
+      
+      Returns:
+        list: A list of strings, where each string contains a number and its surrounding delimiters.
+    """
+
+    return re.split(
+       r'[' + re.escape(delimiters) + r']',
+       numbers
+    )
 
   def add(self, numbers):
     """
-      Adds the sum of all positive integers separated by commas in a given string.
+    Adds the sum of all positive integers separated by commas in a given string.
       Args:
         numbers (str): A string containing numbers separated by commas
           
@@ -16,15 +58,23 @@ class StringCalculator:
       Raises:
           NegativeNumberException: If any number in the input string is negative.
     """
+
+    # remove empty spaces and check empty string
     total = 0
+    numbers = numbers.strip()
     if numbers == "":
       return total
-    numbers = numbers.split(",")
 
+    # Use comma(,) as a default delimiter
+    delimiters = ","
+    # Get delimiter and numbers if it has delimiter
+    if numbers.startswith("//"):
+      delimiters, numbers = self.get_delimiters_and_numbers(numbers)
+
+    numbers = self.split_numbers_with_delimiters(delimiters, numbers)
     for number in numbers:
       number = int(number.strip())
       if number < 0:
         raise NegativeNumberException(number)
       total += number
     return total
-  
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -22,6 +22,13 @@ class TestStringCalculator(unittest.TestCase):
   
   def test_new_lines_multiple_numbers(self):
     self.assertEqual(self.calc.add("1,2\n\n\n,\n5,\n6,3"), 17)
+  
+  def test_custom_delimiters(self):
+    self.assertEqual(self.calc.add("""//,;.[
+              1,2;3;4[5
+              """),
+          15)
+    
 
 if __name__ == "__main_":
   unittest.main()
