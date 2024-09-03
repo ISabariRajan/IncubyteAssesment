1eb7cebe5364a4dad16531e5a96dc04d5200a8fd
Sabari Rajan <iamsabarirajan@gmail.com>
2024-09-03 16:26:18 +0530

    Added Check for negative number and test case

--- calculator.py
+++ calculator.py
@@ -1,21 +1,30 @@
+class NegativeNumberException(Exception):
+    def __init__(self, message="negative numbers not allowed"):
+        super().__init__(message)
 
 class StringCalculator:
 
   def add(self, numbers):
     """
-      Adds the sum of all integers separated by commas in a given string.
+      Adds the sum of all positive integers separated by commas in a given string.
       Args:
-          numbers (str): A string containing numbers separated by commas.
+        numbers (str): A string containing numbers separated by commas
           
       Returns:
-          int: The total sum of all integers in the input string.
+        int: The total sum of all positive integers in the input string.
+          
+      Raises:
+          NegativeNumberException: If any number in the input string is negative.
     """
     total = 0
     if numbers == "":
       return total
     numbers = numbers.split(",")
-    
+
     for number in numbers:
-      total += int(number)
+      number = int(number)
+      if number < 0:
+        raise NegativeNumberException()
+      total += number
     return total
   
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import StringCalculator
+from calculator import StringCalculator, NegativeNumberException
 
 class TestStringCalculator(unittest.TestCase):
   
@@ -15,5 +15,11 @@ class TestStringCalculator(unittest.TestCase):
   def test_add_multiple_numbers(self):
     self.assertEqual(self.calc.add("1,2,5,6,3"), 17)
   
+  def test_negative_number_exception(self):
+    with self.assertRaises(NegativeNumberException) as context:
+        self.calc.add("-1,-2,-3")
+    self.assertTrue("negative numbers not allowed" in str(context.exception))
+  
+
 if __name__ == "__main_":
   unittest.main()
